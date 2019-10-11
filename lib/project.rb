245e1970ffd06eeb35e69e6122a1b8d995330626
Mark Stone <markwstone85@gmail.com>
2019-10-11 00:46:04 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_reader :title, :backers
+
+  def initialize(title, backers = [])
+    @title = title
+    @backers = backers
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+
+end
